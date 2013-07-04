b0525179e38efe8951fbcd1ae956e841ad0dedbc
benoitponsero <b.ponsero@gmail.com>
2013-07-04 17:12:56 +0200

    v0.5

--- code/project/Build.scala
+++ code/project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "devkit"
-  val appVersion      = "1.4"
+  val appVersion      = "1.6"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
